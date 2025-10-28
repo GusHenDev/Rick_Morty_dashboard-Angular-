a0fe79b7e41603d03dc4d06e19b5db513b53a0e8
unknown <gustavosantan10a@gmail.com>
2025-10-28 18:09:43 -0300

    criando o basico

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '',redirectTo:'characters', pathMatch: 'full'},
+    {
+        path: 'characters', 
+        loadComponent: () => import('./features/characters/characters').then(m => m.Characters),
+    },
+    {
+        path: 'locations', 
+        loadComponent: () => import('./features/locations/locations').then(m => m.Locations),
+    },
+    {
+        path: 'episodes', 
+        loadComponent: () => import('./features/episodes/episodes').then(m => m.Episodes),
+    },
+    {path: '**', redirectTo:'characters'}
+];
